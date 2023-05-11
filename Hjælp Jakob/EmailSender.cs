83ec8aed1e6c8d4e3a0fc3b7189a69200722db4d
mart6110 <martinus.mortensen@gmail.com>
2023-05-11 08:15:42 +0200

    Add project files.

---
+++ Hjælp Jakob/EmailSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hjælp_Jakob
+{
+    public class EmailSender
+    {
+        // Sends a message via SMTP
+        public void SendMessage(Message message, bool isHTML)
+        {
+            if (isHTML)
+            {
+                message.Body = ConvertBodyToHTML(message.Body);
+            }
+        }
+
+        // Converts plain text to HTML
+        private string ConvertBodyToHTML(string plainText)
+        {
+            return "<html><body>" + plainText + "</body></html>";
+        }
+    }
+}
---
+++ Hjælp Jakob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hjælp_Jakob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
